89318897a9af3c0af623307d9155d1c093f12abf
Raimundo Silva <raimundo.silvabv@gmail.com>
2022-10-15 18:25:15 -0400

    Ajuste no fluxo de navegação

--- res/escrever_frases.js
+++ res/escrever_frases.js
@@ -1,15 +1,15 @@
 json_data = {
 	"data":[
 		{
-			"frase_resposta":"The secretary writes letters..",
+			"frase_resposta":"Letters are written by the secretary.",
 			"frase_ingles": "The secretary writes letters."
 		},
 		{
-			"frase_resposta":"He saw the boss.",
+			"frase_resposta":"The boss was seen by him.",
 			"frase_ingles": "He saw the boss."
 		},
 		{
-			"frase_resposta":"Maria was offered a new job.",
+			"frase_resposta":"A new job was offered to Maria.",
 			"frase_ingles": "Maria was offered a new job."
 		},
 	]
