5773d7c08f5f4d49d58ec20afa55eca8ad721264
Yuri guilherme <39995851+YuriBoli@users.noreply.github.com>
2022-10-16 22:34:22 -0400

    adicionado presete simples e perfeito

---
+++ res/present_prefect_simple.js
@@ -0,0 +1,12 @@
+json_data = {
+	"data":[
+		{
+			"frase":"I have worked as teacher in the past.",
+			"vos_ativa": true
+		},
+		{
+			"frase":"I worked as a teacher in 1995.",
+			"vos_ativa": true
+		},
+	]
+}
